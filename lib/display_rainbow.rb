f3c446528cce5f690e2a64265794e924987dc23d
Tony Ho <ho.tonym@gmail.com>
2018-04-20 21:57:10 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,14 +1,14 @@
-def color_method(color_array[])
+def color_method(color_array)
   color_array = ["red","orange","yellow","green","blue","indigo","violet"]
   puts "R:#{color_array[0]},O:#{color_array[1]},Y:#{color_array[2]},G:#{color_array[3]},B:#{color_array[4]},I:#{color_array[5]},V:#{color_array[6]}"
-  
+
 end
 
 
-def display_rainbow(color_array[])
+def display_rainbow(color_array)
 
-color_method(color_array[])
+color_method(color_array)
 
 
-  
+
 end
